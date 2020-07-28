7361034010f12809ade990902ef25bd00862d715
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-07-28 10:23:35 +0900

    質問７〜９回答致しました。

--- task.rb
+++ task.rb
@@ -67,7 +67,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array = array.map(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -76,7 +76,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages = programming_languages.map!(&:capitalize)
+  upper_case_programming_languages =  programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -86,6 +87,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name,i|
+    puts "会員No.#{i} #{name}さん"
+  end
 
 end
 
