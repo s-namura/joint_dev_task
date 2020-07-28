7b0871245cdceb1dbb14e06c51def4e9cdf1d8d2
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-07-28 14:45:08 +0900

    質問７，８修正致しました。

--- task.rb
+++ task.rb
@@ -67,16 +67,18 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array = array.map(&:to_i)
+  array = array.map!(&:to_i)
+
   # 以下は変更しないで下さい
   p array
+
 end
 
 def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map!(&:capitalize)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages =  programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
