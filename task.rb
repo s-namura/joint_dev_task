f63ccbedd8a5f8546e3305c1e172e0f122d114c2
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-07-29 14:22:22 +0900

    質問11訂正，質問13〜16回答致します。

--- task.rb
+++ task.rb
@@ -113,9 +113,8 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-sports = sports.flatten.uniq
 
-sports.each.with_index(1) do |sport,i|
+sports.flatten.uniq.each.with_index(1) do |sport,i|
   puts "No#{i} #{sport}"
 end
 
@@ -125,7 +124,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-  #value = data.name[#user]
+  
   puts "ユーザー名: #{data[:user][:name]}"
   
 end
@@ -135,6 +134,8 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
+  
+  puts user_data.merge(update_data)
 
 end
 
@@ -142,6 +143,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
+  
+  p data.keys
 
 end
 
@@ -150,7 +153,9 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.has_key?(:age) ?  "OK" : "NG"
+  puts data2.has_key?(:age) ?  "OK" : "NG"
+  
 end
 
 def q16
@@ -162,6 +167,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 
 end
 
