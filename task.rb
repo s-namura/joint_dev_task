f749d7e653ccda20d27402f1f692fbf2bd0ba2b2
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-07-31 16:54:16 +0900

    質問18回答致しました。

--- task.rb
+++ task.rb
@@ -201,6 +201,17 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def introduce
+    if @age >= 20
+      puts "こんにちは，#{@name}と申します。宜しくお願いいたします。"
+    else
+      puts "はいさいまいど〜，#{@name}です！！！"
+    end  
+  end
 
 end
 
